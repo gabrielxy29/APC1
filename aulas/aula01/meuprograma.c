01554e508eb6575502e588dfd4d42bf0d556eb73
gabrielxy29 <142457635+gabrielxy29@users.noreply.github.com>
2023-08-24 00:51:49 +0000

    aula pratica 1

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,15 @@
+/*
+  Autor: Gabriel Henrique
+  Data: 23/08/2023
+  programa que exibe ola pessoal!
+  */
+
+#include <stdio.h>
+#include <math.h>
+
+//funcao principal
+int main() {
+  printf("Ola pessoal!\n");
+  printf("A raiz quadrade de 2 é %f\n", sqrt(2));
+  return 0;
+}
